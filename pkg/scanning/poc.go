c2f5139f430e3273b1f535acbda0f39b6e00d954
emrekara369 <emrekara369@gmail.com>
2024-07-03 19:41:16 +0000

    added extra control for againist req.Body equal to nil

--- pkg/scanning/poc.go
+++ pkg/scanning/poc.go
@@ -16,32 +16,33 @@ func MakePoC(poc string, req *http.Request, options model.Options) string {
 			return "HTTP RAW REQUEST\n" + string(requestDump)
 		}
 	}
-
-	if req.Body != nil {
-		body, err := req.GetBody()
-		if err == nil {
-			reqBody, err := io.ReadAll(body)
+	if req != nil{
+		if req.Body != nil && req.GetBody != nil{
+			body, err := req.GetBody()
 			if err == nil {
-				if string(reqBody) != "" {
-					switch options.PoCType {
-					case "curl":
-						return "curl -i -k -X " + req.Method + " " + poc + " -d \"" + string(reqBody) + "\""
-					case "httpie":
-						return "http " + req.Method + " " + poc + " \"" + string(reqBody) + "\" --verify=false -f"
-					default:
-						return poc + " -d " + string(reqBody)
+				reqBody, err := io.ReadAll(body)
+				if err == nil {
+					if string(reqBody) != "" {
+						switch options.PoCType {
+						case "curl":
+							return "curl -i -k -X " + req.Method + " " + poc + " -d \"" + string(reqBody) + "\""
+						case "httpie":
+							return "http " + req.Method + " " + poc + " \"" + string(reqBody) + "\" --verify=false -f"
+						default:
+							return poc + " -d " + string(reqBody)
+						}
 					}
 				}
 			}
-		}
-	} else {
-		switch options.PoCType {
-		case "curl":
-			return "curl -i -k " + poc
-		case "httpie":
-			return "http " + poc + " --verify=false"
-		default:
-			return poc
+		} else {
+			switch options.PoCType {
+			case "curl":
+				return "curl -i -k " + poc
+			case "httpie":
+				return "http " + poc + " --verify=false"
+			default:
+				return poc
+			}
 		}
 	}
 	return poc
